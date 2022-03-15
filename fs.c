9a71a286c7397c06ff2b8b67e57d2191be488f9b
Filipe dos Santos Pires <201819050289@lab03-l>
2022-03-15 14:30:16 -0400

    start of fs_mkfs implementation

--- fs.c
+++ fs.c
@@ -5,18 +5,20 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+
 #ifdef FAKE
-#include <stdio.h>
 #define ERROR_MSG(m) printf m;
 #else
 #define ERROR_MSG(m)
 #endif
 
-char* MAGIC_NUMBER = 0x0140c52a;
+char *MAGIC_NUMBER = 0x0140c52a;
 
-void *my_malloc(int size) {
+void *my_malloc(int size)
+{
   void *tmp = malloc(size);
-  if (tmp == NULL) {
+  if (tmp == NULL)
+  {
     fprintf(stderr, "Could not alloc memory\n");
     exit(1);
   }
@@ -24,82 +26,109 @@ void *my_malloc(int size) {
   return tmp;
 }
 
-void my_fseek(void* file, int seek) {
+void my_fseek(void *file, int seek)
+{
   int ret;
   ret = fseek(file, 0, SEEK_SET);
-  assert(ret==0);
+  assert(ret == 0);
   ret = fseek(file, seek, SEEK_SET);
-  assert(ret==seek);
+  assert(ret == seek);
   return;
 }
 
-void fs_init( void) {
+void fs_init(void)
+{
   block_init();
-  FILE* file = fopen("./disk", "r+b");
+  FILE *file = fopen("./disk", "r+b");
   my_fseek(file, SECTOR_SIZE - 4);
-  char* magic_number;
+  char *magic_number;
   int ret = fread(magic_number, 1, 4, file);
-  if (magic_number == MAGIC_NUMBER) {
+  if (magic_number == MAGIC_NUMBER)
+  {
     // mounted
     return;
   }
 
   my_fseek(file, SECTOR_SIZE - 4);
   ret = fwrite(MAGIC_NUMBER, 1, 4, file);
-  assert(ret==4);
+  assert(ret == 4);
   return;
 }
 
-int fs_mkfs( void) {
-    return -1;
+int fs_mkfs(void)
+{
+  FILE *fp;
+  char zero = '0';
+  int i, k, j;
+  char dataFileName[MAX_FILE_NAME];
+  SuperBlock spBlock;
+
+  spBlock.blockSize = 512;
+  spBlock.iNodeCount = 64;
+  // strcpy(dataFileName, argv[3]);
+  spBlock.iNodePositions = spBlock.blockSize + sizeof(SuperBlock); // starting address of iNode
+  spBlock.iNodeSize = sizeof(iNode);
+
+  return -1;
 }
 
-int fs_open( char *fileName, int flags) {
-    return -1;
+int fs_open(char *fileName, int flags)
+{
+  return -1;
 }
 
-int fs_close( int fd) {
-    return -1;
+int fs_close(int fd)
+{
+  return -1;
 }
 
-int fs_read( int fd, char *buf, int count) {
-    return -1;
-}
-    
-int fs_write( int fd, char *buf, int count) {
-    return -1;
+int fs_read(int fd, char *buf, int count)
+{
+  return -1;
 }
 
-int fs_lseek( int fd, int offset) {
-    return -1;
+int fs_write(int fd, char *buf, int count)
+{
+  return -1;
 }
 
-int fs_mkdir( char *fileName) {
-    return -1;
+int fs_lseek(int fd, int offset)
+{
+  return -1;
 }
 
-int fs_rmdir( char *fileName) {
-    return -1;
+int fs_mkdir(char *fileName)
+{
+  return -1;
 }
 
-int fs_cd( char *dirName) {
-    return -1;
+int fs_rmdir(char *fileName)
+{
+  return -1;
 }
 
-int 
-fs_link( char *old_fileName, char *new_fileName) {
-    return -1;
+int fs_cd(char *dirName)
+{
+  return -1;
 }
 
-int fs_unlink( char *fileName) {
-    return -1;
+int fs_link(char *old_fileName, char *new_fileName)
+{
+  return -1;
 }
 
-int fs_stat( char *fileName, fileStat *buf) {
-    return -1;
+int fs_unlink(char *fileName)
+{
+  return -1;
 }
 
-int main() {
+int fs_stat(char *fileName, fileStat *buf)
+{
+  return -1;
+}
+
+int main()
+{
   fs_init();
   return 0;
 }
--- fs.h
+++ fs.h
@@ -2,21 +2,45 @@
 #define FS_INCLUDED
 
 #define FS_SIZE 2048
-
-void fs_init( void);
-int fs_mkfs( void);
-int fs_open( char *fileName, int flags);
-int fs_close( int fd);
-int fs_read( int fd, char *buf, int count);
-int fs_write( int fd, char *buf, int count);
-int fs_lseek( int fd, int offset);
-int fs_mkdir( char *fileName);
-int fs_rmdir( char *fileName);
-int fs_cd( char *dirName);
-int fs_link( char *old_fileName, char *new_fileName);
-int fs_unlink( char *fileName);
-int fs_stat( char *fileName, fileStat *buf);
-
 #define MAX_FILE_NAME 32
-#define MAX_PATH_NAME 256  // This is the maximum supported "full" path len, eg: /foo/bar/test.txt, rather than the maximum individual filename len.
+#define MAX_PATH_NAME 256 // This is the maximum supported "full" path len, eg: /foo/bar/test.txt, rather than the maximum individual filename len.
+
+struct SuperBlock // 24 byte
+{
+    int blockSize;  // +
+    int iNodeCount; // +
+    int blockCount;
+    // int iNodeSize;  //+
+    int iNodePositions; // +
+    int blockPositions; // +
+};
+
+struct iNode // 104 byte
+{
+    int fileType; // 0 = root 1 = file, 2 = folder,3 = softlink, 4 = hardlink
+    int fileSize;
+    char fileName[MAX_FILE_NAME];
+    int parentInode;
+    int inodeNumber;         // 0-> root
+    time_t lastModification; // 8 byte
+    int blocksNum[12];       // assigned to 65535 as unused
+};
+
+typedef iNode iNode;
+typedef SuperBlock SuperBlock;
+
+void fs_init(void);
+int fs_mkfs(void);
+int fs_open(char *fileName, int flags);
+int fs_close(int fd);
+int fs_read(int fd, char *buf, int count);
+int fs_write(int fd, char *buf, int count);
+int fs_lseek(int fd, int offset);
+int fs_mkdir(char *fileName);
+int fs_rmdir(char *fileName);
+int fs_cd(char *dirName);
+int fs_link(char *old_fileName, char *new_fileName);
+int fs_unlink(char *fileName);
+int fs_stat(char *fileName, fileStat *buf);
+
 #endif
